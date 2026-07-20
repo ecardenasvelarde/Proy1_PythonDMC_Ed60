61eb24c41439662b2a214a2e577e9f7516642997
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-07-19 23:01:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ st.sidebar.image('DMC.png')
 app_mode = st.sidebar.selectbox('_Secciones_',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
 
 if app_mode == 'Home':
+  st.write("Bienvenido a la " & app_mode)
 elif app_mode == 'Ejercicio 1':
+  st.write("Bienvenido a la " & app_mode)
 elif app_mode == 'Ejercicio 2':
+  st.write("Bienvenido a la " & app_mode)
 elif app_mode == 'Ejercicio 3':
+  st.write("Bienvenido a la " & app_mode)
 elif app_mode == 'Ejercicio 4':
-  
+  st.write("Bienvenido a la " & app_mode)
