7822316bb507c0157e68d2d2bee222292e25ae42
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-07-19 22:45:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import libreria_funciones_proyecto1 as lfp
 
+st.sidebar.image('DMC.png')
+app_mode = st.sidebar.selectbox('_Secciones_',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
