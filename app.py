64f59e31e0ad633e2715d06e31ca3940cdb83110
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-07-19 23:19:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,21 @@ app_mode = st.sidebar.selectbox('_Secciones_',['Home','Ejercicio 1','Ejercicio 2
 
 if app_mode == 'Home':
   st.title ('Proyecto N°1 de la Especialización de Python Ed. 60')
-  st.subheader("_Streamlit_ is :red[cool] :sunglasses:")
   st.image('Python_logo.png')
-  st.write("Elaborado por: Erick Cardenas")
+  st.subheader("_Streamlit_ is :red[cool] :sunglasses:")
+  
+  st.write("Elaborado por: Erick Cárdenas")
   st.markdown(
     '''
-    Estudiante: Miguel Angel Limaquispe Huaman
+    Estudiante: Erick Eduardo Cárdenas Velarde.
     
-    Modulo 1: Pyhton Fundamentals
+    Modulo 1: Pyhton Fundamentals.
     
-    Información General: Ingeniero de Sistemas con experiencia en Bussines Intelligence en Sectores de Banca, Telecomunicaciones, Consumo Masivo y Farmaceútico.
+    Información General: Ingeniero Informático con experiencia en automatización de procesos, actualmente trabajando en el Backoffice de un Banco.
     
-    Año: 34 años
+    Año: 2026.
     
-    Descripción: Proyecto enfocado en el desarrollo de una Aplicación con Streamlit
+    Descripción: Proyecto enfocado en el desarrollo de una Aplicación con Streamlit.
     '''
     )
 elif app_mode == 'Ejercicio 1':
