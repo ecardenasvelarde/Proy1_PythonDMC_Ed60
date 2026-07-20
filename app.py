a05e417159be86369bfc46fbe429dbf548cdaaea
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-07-19 22:47:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ import libreria_funciones_proyecto1 as lfp
 
 st.sidebar.image('DMC.png')
 app_mode = st.sidebar.selectbox('_Secciones_',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
+
+if app_mode == 'Home':
+elif app_mode == 'Ejercicio 1':
+elif app_mode == 'Ejercicio 2':
+elif app_mode == 'Ejercicio 3':
+elif app_mode == 'Ejercicio 4':
