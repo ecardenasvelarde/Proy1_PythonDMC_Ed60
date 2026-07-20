c899d6b4f9364667ece2859357aafa9b27187420
ecardenasvelarde <e.cardenas.velarde@gmail.com>
2026-07-19 23:12:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,22 @@ st.sidebar.image('DMC.png')
 app_mode = st.sidebar.selectbox('_Secciones_',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
 
 if app_mode == 'Home':
-  st.write("Bienvenido a la " + app_mode)
+  st.title ('Proyecto N°1 de la Especialización de Python')
+  st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+  st.image('Python_logo.png')
+  st.markdown(
+    '''
+    Estudiante: Miguel Angel Limaquispe Huaman
+    
+    Modulo 1: Pyhton Fundamentals
+    
+    Información General: Ingeniero de Sistemas con experiencia en Bussines Intelligence en Sectores de Banca, Telecomunicaciones, Consumo Masivo y Farmaceútico.
+    
+    Año: 34 años
+    
+    Descripción: Proyecto enfocado en el desarrollo de una Aplicación con Streamlit
+    '''
+    )
 elif app_mode == 'Ejercicio 1':
   st.write("Bienvenido a la " + app_mode)
 elif app_mode == 'Ejercicio 2':
